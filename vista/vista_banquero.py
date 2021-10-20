c6a14aed3798e9bfe42e683b072d69e28b40583f
Johan Restrepo <johanesteban.restrepo@utp.edu.co>
2021-10-20 13:41:38 -0500

    Disposicion basica de la vista

--- vista/vista_banquero.py
+++ vista/vista_banquero.py
@@ -5,8 +5,20 @@ class Ventana(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.master.title('Ejemplo de ventana')
+        # self.master.geometry("400x400")
         self.crear_widgets()
 
     def crear_widgets(self):
-        self.boton_salida = tk.Button(self, text='Quit', command=self.quit).grid(row=0, column=1)
-        self.lbl_existentes = tk.Label(self, text='Rescursos existentes').grid(row=0, column=0)
+        self.etiqueta_exitentes = tk.Label(self, text='Rescursos existentes').grid(row=0, column=0, columnspan=2)
+        self.entrada_exitentes = tk.Entry(self).grid(row=1, column=0, columnspan=2)
+
+        self.etiqueta_asignados = tk.Label(self, text='Recursos asignados').grid(row=2, column=0)
+        self.entrada_asignados = tk.Entry(self).grid(row=3, column=0)
+
+        self.etiqueta_solicitados = tk.Label(self, text='Recursos solicitados').grid(row=2, column=1)
+        self.entrada_solicitados = tk.Entry(self).grid(row=3, column=1)
+
+        self.boton_correr = tk.Button(text='Correr').grid(row=4, column=0)
+        self.boton_Ayuda = tk.Button(text='Ayuda').grid(row=4, column=1)
+
+        self.texto_resultado = tk.Text(self, height=2, width=50).grid(row=5, columnspan=2)
